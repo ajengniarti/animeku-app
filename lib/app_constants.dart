d7e13025af0812c19cf2ffab145cbc0253972a72
ajengniarti <201111007@mhs.stiki.ac.id>
2023-05-08 00:05:25 +0700

    inisial dan setup project

---
+++ lib/app_constants.dart
@@ -0,0 +1,7 @@
+class AppConstants {
+  
+  static const String baseUrl = 'https://anime-db.p.rapidapi.com/anime';
+  static const String imageUrl = 'https://cdn.myanimelist.net/images/anime/1337/99013.jpg';
+  static const String apiKey = '66521921b8msh65919cdd8249946p1f754ajsna693dea94a7b';
+  static const String apiHost = 'anime-db.p.rapidapi.com';
+}
